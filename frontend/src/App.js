d872c04aa476b700e3a38058ca16b6bd57e46180
Raza Saleemi <rks143@gmail.com>
2024-09-03 09:10:17 +0500

    feat(initial): Setting up the project and adding react-bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/header';
+import Footer from './components/footer';
+function App() {
+  return (
+    <div>
+      {' '}
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
